f54182363672438446a06e6d8af21d77b5366b35
houssem.ha <houssem.hammami@esprit.tn>
2021-01-28 22:26:52 +0100

    BA + Comment

--- BookStore/src/main/java/com/esprit/project/services/CommentaireService.java
+++ BookStore/src/main/java/com/esprit/project/services/CommentaireService.java
@@ -2,9 +2,13 @@ package com.esprit.project.services;
 
 import com.esprit.project.entities.Commentaire;
 
-import java.util.List;
+
 
 public interface CommentaireService {
 
-    List<Commentaire> getByBookPId(Long id);
+    Commentaire saveCommentaire(Commentaire commentaire);
+    Commentaire updateCommentaire(Commentaire commentaire);
+    void deleteCommentaire(Long id);
+    // List<Commentaire> getByBookPId(Long id);
+
 }
--- BookStore/src/main/java/com/esprit/project/services/impl/CommentaireServiceImpl.java
+++ BookStore/src/main/java/com/esprit/project/services/impl/CommentaireServiceImpl.java
@@ -4,9 +4,9 @@ import com.esprit.project.entities.Commentaire;
 import com.esprit.project.repositories.CommentaireRepository;
 import com.esprit.project.services.CommentaireService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class CommentaireServiceImpl implements CommentaireService {
@@ -14,9 +14,25 @@ public class CommentaireServiceImpl implements CommentaireService {
     @Autowired
     private CommentaireRepository commentaireRepository;
 
+    @Override
+    public Commentaire saveCommentaire(Commentaire commentaire) {
+        return commentaireRepository.save(commentaire);
+    }
 
     @Override
-    public List<Commentaire> getByBookPId(Long id) {
-        return (List<Commentaire>) commentaireRepository.findByBookP_Id(id);
+    public Commentaire updateCommentaire(Commentaire commentaire) {
+        return commentaireRepository.save(commentaire);
     }
+
+
+    @Override
+    public void deleteCommentaire(Long id) {
+        try {
+            commentaireRepository.deleteById(id);
+        } catch (
+                EmptyResultDataAccessException e) {
+            System.out.println("doesn't exist");
+        }
+    }
+
 }
