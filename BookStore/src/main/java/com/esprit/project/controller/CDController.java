9199c98356d67af99000d2fe3b6cf50032e4f9f7
maheromrani <maher.omrani@esprit.tn>
2021-01-28 20:04:26 +0100

    cd et client Controllers

--- BookStore/src/main/java/com/esprit/project/controller/CDController.java
+++ BookStore/src/main/java/com/esprit/project/controller/CDController.java
@@ -1,5 +1,33 @@
 package com.esprit.project.controller;
 
-public class CDController {
+import com.esprit.project.entities.BookPhysique;
+import com.esprit.project.entities.CD;
+import com.esprit.project.services.CDService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/cd/")
+public class CDController {
+    @Autowired
+    private CDService cdService;
+    @GetMapping("All")
+    public Object getAll()
+    {
+        List<CD> cdList = cdService.findAll();
+        return ResponseEntity.status(HttpStatus.FOUND).body(cdList);
+    }
+    @GetMapping("{id}")
+    public Object getById(@PathVariable Long id)
+    {
+        CD cd = cdService.findById(id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(cd);
+    }
 }
--- BookStore/src/main/java/com/esprit/project/controller/ClientController.java
+++ BookStore/src/main/java/com/esprit/project/controller/ClientController.java
@@ -1,5 +1,41 @@
 package com.esprit.project.controller;
 
-public class ClientController {
+import com.esprit.project.entities.Client;
+import com.esprit.project.exceptions.SavingIdException;
+import com.esprit.project.services.CDService;
+import com.esprit.project.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/client/")
+public class ClientController {
+    @Autowired
+    private ClientService clientService;
+    @PostMapping("addclient")
+    public ResponseEntity<?> save(@RequestBody Client client ){
+
+
+        if(client.getId() == null){
+
+            Optional<Client> client1 = Optional.of(clientService.save(client));
+            return ResponseEntity.created(null).body(client1);
+        } else {
+            throw new SavingIdException(HttpStatus.BAD_REQUEST, "entity id must be null");
+        }
+
+    }
+    @PutMapping("update")
+    public ResponseEntity<?> update(@RequestBody Client client){
+        if(client.getId() != null){
+            Client client1 = clientService.save(client);
+            return ResponseEntity.accepted().body(client1);
+        } else {
+            throw new SavingIdException(HttpStatus.BAD_REQUEST, "entity id cannot be null");
+        }
+    }
 }
